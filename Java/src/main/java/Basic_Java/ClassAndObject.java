b302ab205b49454b0ddaa6496bdc39fb8f072197
techtuh <tech.worldevaluation@gmail.com>
2023-07-19 21:59:39 -0400

    done by tuhin

---
+++ Java/src/main/java/Basic_Java/ClassAndObject.java
@@ -0,0 +1,19 @@
+package Basic_Java;
+
+public class ClassAndObject {
+	 
+	public void getMoney() {
+		
+		
+	}
+	
+	
+	public static void main(String[] args) {
+		int a = 4;
+		int b = 5;
+
+		System.out.println(a+b);
+
+	}
+
+}
---
+++ Java/src/main/java/ap/LoginWithResd.java
@@ -0,0 +1,7 @@
+package ap;
+
+public class LoginWithResd {
+	
+	
+
+}
